96aabc9ea75ae0321c049057d1b7e86606b260e7
Oleksandr <oaleks262@gmail.com>
2025-02-04 15:27:56 +0200

    fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Використання статичних файлів (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, "../public")));
